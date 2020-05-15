bdb1b7dcc60c7877c17395652cf3ea41595576f0
Zoila <zoilavillatoro6694@outlook.com>
2020-05-15 11:01:17 -0600

    create,drop and show table

--- main.py
+++ main.py
@@ -13,11 +13,31 @@ PASSWORD = os.getenv('DB_PASSWORD')
 DATABASE = os.getenv('DB_NAME')
 
 
+USER_TABLE= """ CREATE TABLE users(
+				id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+				username VARCHAR(50) NOT NULL,
+				password VARCHAR(50) NOT NULL
+			)"""
+
+DROP_USER = "DROP TABLE IF EXISTS `users`"
+SHOW_TABLES = "SHOW TABLES"
 if __name__ == '__main__':
 	try:
 		connection = MySQLdb.connect(HOST, USER, PASSWORD, DATABASE )
 
 		cursor = connection.cursor()
+		#eliminando tabla
+		cursor.execute(DROP_USER)
+		#creando tabla
+		cursor.execute(USER_TABLE)
+		#mostrando tabla
+		cursor.execute(SHOW_TABLES)
+		tables = cursor.fetchall()
+
+		for table in tables:
+			#print(table)
+			print(table[0])
+
 
 		# ESTAS CONECCIONES DEBEN CERRARSE
 		connection.close()
