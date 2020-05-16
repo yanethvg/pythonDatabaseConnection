56aca75c86233fac522552fe606748508e8d652c
Zoila <zoilavillatoro6694@outlook.com>
2020-05-16 15:52:46 -0600

    added insert and select in table

--- main.py
+++ main.py
@@ -21,6 +21,8 @@ USER_TABLE= """ CREATE TABLE users(
 
 DROP_USER = "DROP TABLE IF EXISTS `users`"
 SHOW_TABLES = "SHOW TABLES"
+INSERT_USER = "INSERT INTO users (username,password) VALUES ('{username}','{password}')"
+SELECT_USER = "SELECT * FROM users WHERE id ={id} "
 if __name__ == '__main__':
 	try:
 		connection = MySQLdb.connect(HOST, USER, PASSWORD, DATABASE )
@@ -31,14 +33,38 @@ if __name__ == '__main__':
 		#creando tabla
 		cursor.execute(USER_TABLE)
 		#mostrando tabla
+		"""
 		cursor.execute(SHOW_TABLES)
 		tables = cursor.fetchall()
 
 		for table in tables:
 			#print(table)
 			print(table[0])
+		"""
+		username = input("ingrese el username ")
+		password = input("ingrese password ")
 
+		query = INSERT_USER.format(username=username, password=password)
+		print(query)
+		try:
+			cursor.execute(query)
+			#para que persista dentro de la base de datos
+			connection.commit()
+		except:
+			#coloque a un estado anterior a la sesion
+			connection.rollback()
 
+		query = SELECT_USER.format(id=1)
+		print(query)
+		cursor.execute(query)
+		users = cursor.fetchall()
+		print(users[0])
+
+		#para ver la informacion
+		"""
+		for user in users:
+			print(user)
+		"""
 		# ESTAS CONECCIONES DEBEN CERRARSE
 		connection.close()
 	except MySQLdb.Error as error:
